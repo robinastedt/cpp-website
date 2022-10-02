f1a270f54b54f9eac04012a4961a542d99ed1577
Robin Åstedt <robin.astedt@gmail.com>
2022-10-02 13:50:25 +0200

    Use cmake namespaces and object libraries.

---
+++ src/cmake/targets.cmake
@@ -0,0 +1,34 @@
+
+function(add_namespaced_library)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "NAME" "SOURCES;DEPENDS_INTERNAL;DEPENDS")
+
+    add_library(${PROJECT_NAME}_${ARG_NAME}_obj OBJECT ${ARG_SOURCES})
+    add_library(${PROJECT_NAME}::${ARG_NAME}_obj ALIAS ${PROJECT_NAME}_${ARG_NAME}_obj)
+
+    add_library(${PROJECT_NAME}_${ARG_NAME} INTERFACE)
+    add_library(${PROJECT_NAME}::${ARG_NAME} ALIAS ${PROJECT_NAME}_${ARG_NAME})
+
+    list(TRANSFORM ARG_DEPENDS_INTERNAL REPLACE "(.+)" "${PROJECT_NAME}::\\1")
+    target_link_libraries(${PROJECT_NAME}_${ARG_NAME}_obj PRIVATE
+        ${ARG_DEPENDS_INTERNAL}
+        ${ARG_DEPENDS}
+    )
+
+    target_link_libraries(${PROJECT_NAME}_${ARG_NAME} INTERFACE
+        ${PROJECT_NAME}::${ARG_NAME}_obj
+        $<TARGET_OBJECTS:${PROJECT_NAME}::${ARG_NAME}_obj>
+    )
+endfunction()
+
+function(add_namespaced_executable)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "NAME" "SOURCES;DEPENDS_INTERNAL;DEPENDS")
+
+    add_executable(${ARG_NAME} ${ARG_SOURCES})
+    add_executable(${PROJECT_NAME}::${ARG_NAME} ALIAS ${ARG_NAME})
+
+    list(TRANSFORM ARG_DEPENDS_INTERNAL REPLACE "(.+)" "${PROJECT_NAME}::\\1")
+    target_link_libraries(${ARG_NAME} PRIVATE
+        ${ARG_DEPENDS_INTERNAL}
+        ${ARG_DEPENDS}
+    )
+endfunction()
